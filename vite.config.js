e6d276cd34a92aedccc8679e02b25b44f3401353
Sourav Das <dikisourav@gmail.com>
2026-03-17 04:27:08 +0530

    changed base in vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "tiffincurry-website",
+  base: "/tiffincurry-website",
   server: {
     port: 3000
   }
