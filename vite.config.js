2866f439de7ceb50dd3316181b0b828dbe37ec0c
Sourav Das <dikisourav@gmail.com>
2026-03-17 04:35:00 +0530

    removed port and changed base in vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,8 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/tiffincurry-website",
-  server: {
-    port: 3000
-  }
+  base: "/tiffincurry-website/"
 })
