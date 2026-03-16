3d228129e458e939e09d360a15388840083fe872
Sourav Das <dikisourav@gmail.com>
2026-03-17 04:42:35 +0530

    added base in vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,8 +3,8 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()]//,
-  // base: "/tiffincurry-website",
+  plugins: [react()],
+   base: "/tiffincurry-website",
   // server: {
   //   port: 3000
   // }
