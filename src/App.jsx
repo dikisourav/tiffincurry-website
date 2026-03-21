903cd0dbcbb9a17ad571cf73e22e31d0b4a200b9
Sourav Das <dikisourav@gmail.com>
2026-03-22 02:30:26 +0530

    added the page open and scroll to top component

--- src/App.jsx
+++ src/App.jsx
@@ -3,23 +3,26 @@ import Home from "./pages/Home"
 import Demo from "./pages/Demo"
 import Pricing from "./pages/Pricing"
 import About from "./pages/About"
+import ScrollToTop from "./components/ScrollToTop";
+
 
 export default function App(){
 
 return(
+<>
+    <ScrollToTop/>
+    <Routes>
+        
+        <Route path="/tiffincurry-website" element={<Home/>} />
 
-<Routes>
+        <Route path="/tiffincurry-website/demo" element={<Demo/>} />
 
-<Route path="/tiffincurry-website" element={<Home/>} />
+        <Route path="/tiffincurry-website/pricing" element={<Pricing/>} />
 
-<Route path="/tiffincurry-website/demo" element={<Demo/>} />
+        <Route path="/tiffincurry-website/about" element={<About/>} />
 
-<Route path="/tiffincurry-website/pricing" element={<Pricing/>} />
-
-<Route path="/tiffincurry-website/about" element={<About/>} />
-
-</Routes>
-
+    </Routes>
+</>
 )
 
 }
