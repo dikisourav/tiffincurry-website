6830672181aef3c6291ec53a20542f0b90cbae45
Sourav Das <dikisourav@gmail.com>
2026-04-06 23:43:41 +0530

    added feature- Guest First Approach

--- src/components/Features.jsx
+++ src/components/Features.jsx
@@ -2,6 +2,7 @@ import Feature from "./Feature";
 import hrm from "../assets/hrm.png";
 import pos from "../assets/lightning-fast-billing.png";
 import kot from "../assets/kot_screen.png";
+import clp from "../assets/customer_centricity.png";
 export default function Features(){
 return(
 <section className="features reveal reveal-stagger">
@@ -20,19 +21,27 @@ return(
     text="Process orders instantly with a touch-friendly interface designed for high-volume restaurants."
     img={pos}
     />
+
+    <Feature
+    title="Guest first approach"
+    text="Enhance customer satisfaction with personalized service and seamless interactions at every touchpoint."
+    img={clp}
+    reverse
+    />
     
     <Feature
     title="Smart kitchen display"
     text="Orders reach the kitchen instantly so chefs can prepare dishes faster."
     img={kot}
-    reverse
     />
     
     <Feature
     title="Powerful HRM"
     text="Manage your team effectively with our comprehensive human resources management system."
     img={hrm}
+    reverse
     />
+
     </div>
 </section>
 )
